c4c539f31f92b17bb8b7a0cf91129e71991876fe
clfurman <crystal.furman@gmail.com>
2024-03-01 10:38:19 -0500

    add SIGCSEMeteor2.java

--- src/main/java/org/yourcompany/yourproject/SIGCSEMeteor2.java
+++ src/main/java/org/yourcompany/yourproject/SIGCSEMeteor2.java
@@ -14,49 +14,41 @@ import java.util.Scanner;
  */
 public class SIGCSEMeteor2 {
 
-    public static ArrayList<Meteor> readData (String filename)
-    {
-        ArrayList<Meteor> meteors = new ArrayList <Meteor>();
+    private static ArrayList<Meteor> readData (String filename) {
+        ArrayList<Meteor> meteors = new ArrayList <>();
 
         File dataFile = new File (filename);
 
-        try (Scanner fileScanner = new Scanner(dataFile))
-        {
-            ArrayList<String> lines = new ArrayList<String>();
+        try (Scanner fileScanner = new Scanner(dataFile)) {
+            ArrayList<String> lines = new ArrayList<>();
 
-            while (fileScanner.hasNextLine())
-            {
+            while (fileScanner.hasNextLine()) {
                 lines.add(fileScanner.nextLine());
             }
-            
-            for (int index = 1; index < lines.size(); index++) // skips header row if you start at 1. 
-            {
+            // loop index starts at 1 to skip the header row 
+            for (int index = 1; index < lines.size(); index++) {  
                 String l = lines.get(index);
 
                 String[] data = l.split (",");
-                if (data.length > 9 )
-                {
+                if (data.length > 9 ) {
                                  
-                Meteor m = new Meteor (data[0], Integer.parseInt(data[1]), data[2], data[3], 
+                    Meteor m = new Meteor (data[0], Integer.parseInt(data[1]), data[2], data[3], 
                                               Double.parseDouble(data[4]), data[5], data[6], 
                                                 new GeoLocation (Double.parseDouble(data[7]), Double.parseDouble(data[8])));
                 
                 
-                meteors.add(m);
-               
+                    meteors.add(m);
                 }
             }
         }
-        catch (FileNotFoundException error)
-        {
+        catch (FileNotFoundException error) {
             handleError (error);
         }
         
         return meteors;
     }
 
-    public static void handleError(Exception error)
-    {
+    public static void handleError(Exception error) {
         System.out.println ("The following error occured: ");
         System.out.println (error.getMessage());
     }
@@ -65,10 +57,9 @@ public class SIGCSEMeteor2 {
         System.out.println ("Hi!");
 
         ArrayList <Meteor> myMeteors = readData ("C:\\Users\\Crystal Furman\\SIGCSEMeteor2\\src\\main\\java\\org\\yourcompany\\yourproject\\Meteorite_Landings.csv");
-        ArrayList <Meteor> lowMassMeteors = new ArrayList<Meteor>();
+        ArrayList <Meteor> lowMassMeteors = new ArrayList<>();
         System.out.println ("All done reading");
-        for (int i = 0; i < 10 && i < myMeteors.size(); i++)
-        {
+        for (int i = 0; i < 10 && i < myMeteors.size(); i++) {
             if (5000 > myMeteors.get(i).mass()) {
                 lowMassMeteors.add(myMeteors.get(i));
 
